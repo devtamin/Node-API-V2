fc44784a1fc74e6ba4f93fccaf0702cff1d4bdf6
Alex <phithounsavanh@gmail.com>
2023-06-20 14:05:57 -0400

    finish node api version 2

---
+++ server.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const proudctRoute = require('./routes/productRoute');
+const errorMiddleware = require('./middleware/errorMiddleware')
+var cors = require('cors')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+const MONGO_URL = process.env.MONGO_URL
+const FRONTEND = process.env.FRONTEND
+
+var corsOptions = {
+    origin: FRONTEND,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//routes
+
+app.use('/api/products', proudctRoute);
+
+app.get('/', (req, res) => {
+    res.send('Hello NODE API')
+})
+
+app.get('/blog', (req, res) => {
+    res.send('Hello Blog, My name is Devtamin')
+})
+
+app.use(errorMiddleware);
+
+mongoose.set("strictQuery", false)
+mongoose.
+connect(MONGO_URL)
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(PORT, ()=> {
+        console.log(`Node API app is running on port ${PORT}`)
+    });
+}).catch((error) => {
+    console.log(error)
+})
